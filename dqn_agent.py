97531683a406c4a2d0c70bd7ff0fadd5a3ff26a7
Luis Da Costa <dacosta.le@gmail.com>
2019-02-22 15:40:10 -0500

    First implementation of the agent

---
+++ dqn_agent.py
@@ -0,0 +1,87 @@
+import numpy as np
+import torch
+import random
+from torch import optim
+
+from model import QNetwork
+from memory import ReplayBuffer
+
+
+class Agent():
+    """General agent that interacts with and learns from the environment."""
+
+    def __init__(self,
+                 state_size, action_size,
+                 lr=1e-3, batch_size=64,
+                 update_every_steps=10,
+                 seed=0, device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu")):
+        """
+        Initialization
+        :param state_size: how many states in world.
+        :param action_size: how many agents can the agent choose from.
+        :param seed: to reproduce results.
+        :param device: do I have a GPU, or not?
+        """
+        self.state_size = state_size
+        self.action_size = action_size
+        self.seed = random.seed(seed)
+        self.device = device
+        self.update_every_steps = update_every_steps
+        self.qnetwork_local = QNetwork(state_size=self.state_size, action_size=self.action_size, seed=seed).to(self.device)
+        self.qnetwork_target = QNetwork(state_size=self.state_size, action_size=self.action_size, seed=seed).to(self.device)
+        self.optimizer = optim.Adam(params=self.qnetwork_local.parameters(), lr=lr)
+        self.memory = ReplayBuffer(buffer_size=1e3, batch_size=batch_size)
+        # let's keep track of the steps so that we can run the algorithms properly
+        self.t_step = 0
+
+    def step(self, state, action, reward, next_state, done):
+        """ One full interaction with the environment. """
+        self.memory.add(state, action, reward, next_state, done)
+        self.t_step += 1
+        if self.t_step % self.update_every_steps == 0:
+            if len(self.memory) >= self.memory.batch_size:
+                self.learn(self.memory.sample())
+
+    def learn(self, experiences, gamma=0.9, tau=1e-3):
+        """
+
+        :param experiences:
+        :param gamma:
+        :return:
+        """
+        # unpack:
+        states, actions, rewards, next_states, dones = experiences
+        # let's see what is the expected returns of next_states, and take the max of them:
+        expected_next = self.qnetwork_local(states).detach()  # I don't want toe computation graph to keep track of this
+        expected_next = expected_next.max(1)[0]  # max values
+        expected_next = expected_next.unsqueeze(1)  # values in columns
+        targets = rewards + (gamma * expected_next * dones)  # in case there is NO 'next'
+        # ok, now optimize my network:
+        self.qnetwork_local.do_optimization_step(self.optimizer, states_seen=states, actions_taken=actions, targets=targets)
+
+        # let's soft-copy the values onto the target network:
+        # target_weights = tau * local_weights = (1 - tau)*target_weights
+        for target_param, local_param in zip(self.qnetwork_target.parameters(), self.qnetwork_local.parameters()):
+            target_param.data.copy_(tau * local_param.data + (1.0 - tau) * target_param.data)
+
+    def act(self, state, eps=0.1):
+        """
+
+        :param state: state(s) to be evaluated. Can be in numpy or as tensors.
+        :param eps: value in [0,1] for epsilon-greedy choice.
+        :return: a numpy array with the actions to take
+        """
+        # epsilon-greedy choice:
+        if random.random() < eps:
+            return random.choice(range(self.action_size)) # or np.arange? Does it make a difference?
+        # if state is a numpy array, transform it into a tensor:
+        if isinstance(state, np.ndarray):
+            state = torch.from_numpy(state).float().unsqueeze(0).to(self.device)
+        # let's put the nn into eval mode:
+        self.qnetwork_local.eval()
+        with torch.no_grad():
+            action_values = self.qnetwork_local(state)
+        # let's get it back as training mode:
+        self.qnetwork_local.train()
+        # and now let's take the max of this, and return it as a numpy array:
+        return np.argmax(action_values.cpu().data.numpy())
